8a5055986bcf135853971e85ce1551247ad87689
Pavel <pavel.mokr2005@gmail.com>
2025-04-05 22:13:29 +0500

    Saw4ukAnimator

---
+++ Assets/Scripts/Saw4ukAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Saw4ukAnimator : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private List<Sprite> sprites;
+        [SerializeField] private float secondsBetweenSprites;
+
+        private int cadrNumber = 0;
+
+        private void Awake()
+        {
+            StartCoroutine(Coroutine());
+        }
+
+        private IEnumerator Coroutine()
+        {
+            while (true)
+            {
+                spriteRenderer.sprite = sprites[cadrNumber];
+                cadrNumber += 1;
+                if (cadrNumber == sprites.Count)
+                    cadrNumber = 0;
+                yield return new WaitForSeconds(secondsBetweenSprites);
+            }
+        }
+    }
+}
