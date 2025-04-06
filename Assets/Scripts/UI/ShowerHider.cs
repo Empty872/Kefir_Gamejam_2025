a3c17a6938df6151ce1464d0b94f9b7909b41e6c
Pavel <pavel.mokr2005@gmail.com>
2025-04-06 17:58:40 +0500

    sofgjsdlfkjdf

---
+++ Assets/Scripts/UI/ShowerHider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace.UI
+{
+    public class ShowerHider : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        
+        public void Show()
+        {
+            animator.SetTrigger("Show");
+        }
+    }
+}
