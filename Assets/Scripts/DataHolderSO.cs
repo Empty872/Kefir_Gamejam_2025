551bb38887244f940a026c75484118cc11a6e173
Empty872 <tereshin.vova04@gmail.com>
2025-04-06 15:52:01 +0500

    Added Shahids

--- Assets/Scripts/DataHolderSO.cs
+++ Assets/Scripts/DataHolderSO.cs
@@ -7,10 +7,12 @@ using UnityEngine.Serialization;
 public class DataHolderSO : ScriptableObject
 {
     [SerializeField] private List<Person> people;
+    [SerializeField] private List<Person> shahids;
     [SerializeField] private List<int> possibleWindSpeeds;
     [SerializeField] private List<int> possibleDistances;
     
     public List<Person> People => people;
+    public List<Person> Shahids => shahids;
     public List<int> PossibleWindSpeeds => possibleWindSpeeds;
     public List<int> PossibleDistances => possibleDistances;
 }
