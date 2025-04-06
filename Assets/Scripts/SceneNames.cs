de0f78813f21e85920541198feb106b7d46aa9fc
Empty872 <tereshin.vova04@gmail.com>
2025-04-06 18:05:04 +0500

    Created Win and Loose Scenes

--- Assets/Scripts/SceneNames.cs
+++ Assets/Scripts/SceneNames.cs
@@ -7,4 +7,6 @@ public static class SceneNames
     public static string MainMenuScene = "MainMenuScene";
     public static string TutorialScene = "TutorialScene";
     public static string GameScene = "GameScene";
+    public static string WinScene = "WinScene";
+    public static string LooseScene = "LooseScene";
 }
--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,13 +5,29 @@ namespace DefaultNamespace
 {
     public class SceneTransition : MonoBehaviour
     {
+        public void LoadMenuScene()
+        {
+            SceneManager.LoadScene(SceneNames.MainMenuScene);
+        }
+
         public void LoadGameScene()
         {
             SceneManager.LoadScene(SceneNames.GameScene);
         }
+
         public void LoadTutorialScene()
         {
             SceneManager.LoadScene(SceneNames.TutorialScene);
+        }
+
+        public void LoadWinScene()
+        {
+            SceneManager.LoadScene(SceneNames.WinScene);
+        }
+
+        public void LoadLooseScene()
+        {
+            SceneManager.LoadScene(SceneNames.LooseScene);
         }
     }
 }
