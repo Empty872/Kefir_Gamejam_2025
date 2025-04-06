69e3f33c08372332d858e3dff4b899e0aeff8c4f
Pavel <pavel.mokr2005@gmail.com>
2025-04-06 15:51:18 +0500

    First Part Education

--- Assets/Scripts/Game/IraqController.cs
+++ Assets/Scripts/Game/IraqController.cs
@@ -8,6 +8,7 @@ namespace Orderer
     public class IraqController : MonoBehaviour
     {
         [SerializeField] private List<Person> shahids;
+        [SerializeField] private IraqTextPrinter iraqTextPrinter;
         private int deathCount;
         public UnityEvent onFirstDeath;
         public UnityEvent onSecondDeath;
@@ -39,6 +40,8 @@ namespace Orderer
             {
                 shahid.Died += ShahidOnDied;
             }
+            Environment.Instance.SetWind(0);
+            Environment.Instance.SetDistance(50);
         }
 
         private void ShahidOnDied()
@@ -49,17 +52,20 @@ namespace Orderer
         private void OnFirstDeath()
         {
             Environment.Instance.SetWind(2);
+            iraqTextPrinter.WriteText("Погодные условия изменились. Теперь вы должны брать поправку на ветер. Мы вывели её на ваш супер технологичный прицел, возьмите нужное отклонение по горизонтали и устраните еще одну цель");
             onFirstDeath.Invoke();
         }
 
         private void OnSecondDeath()
         {
             Environment.Instance.SetDistance(150);
+            iraqTextPrinter.WriteText("Теперь нужно взять поправку на дальность. Дистанцию до цели мы тоже вывели на ваш супер технологичный прицел. Возьмите поправку по вертикали и устраните следующую цель");
             onSecondDeath.Invoke();
         }
 
         private void OnThirdDeath()
         {
+            iraqTextPrinter.WriteText("Молодец, капрал, теперь ты должен ликвидировать цель в оживленном районе города. С тобой свяжутся и передадут параметры цели. Не всем сведениям можно верить, учти, что наши информаторы очень тупые.");
             onThirdDeath.Invoke();
         }
     }
---
+++ Assets/Scripts/Game/IraqTextPrinter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Orderer
+{
+    public class IraqTextPrinter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text textField;
+        [SerializeField] private float timeBetweenLettersInSeconds;
+
+        public UnityEvent textWritten;
+
+        private void Awake()
+        {
+            var test = textField.text;
+            textField.text = "";
+            WriteText(test);
+        }
+
+        public void WriteText(string text)
+        {
+            StopAllCoroutines();
+            StartCoroutine(WritingTextCoroutine(text));
+        }
+
+        private IEnumerator WritingTextCoroutine(string text)
+        {
+            var builder = new StringBuilder();
+            foreach (var chr in text)
+            {
+                builder.Append(chr);
+                textField.text = builder.ToString();
+                yield return new WaitForSeconds(timeBetweenLettersInSeconds);
+            }
+            textWritten.Invoke();
+        }
+    }
+}
